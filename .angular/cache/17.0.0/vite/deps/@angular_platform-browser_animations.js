caf3824260fb01486c076bc165298b70634a016b
Gnyblast <guneysaramali@live.co.uk>
2024-03-12 14:36:26 +0200

    Main and alternate titles.

---
+++ .angular/cache/17.0.0/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,21 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-T4TBS5KI.js";
+import "./chunk-4EQVOVK6.js";
+import "./chunk-4VXDH2YL.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-RPP6SJX5.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
