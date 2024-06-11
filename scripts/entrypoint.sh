597fb856437a8e80196d3427a141c10dd39d03da
Güney Saramalı <guneysaramali@live.co.uk>
2024-06-11 22:01:27 +0300

    Fixing the entrypoint for containers

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,4 +6,9 @@ if [ -z "${COMMAND}" ]; then
     COMMAND="start"
 fi
 
-cp -r /var/app/node_modules /app/ && npm run "${COMMAND}"
+if [ ! -d "/app/node_modules" ]; then
+    cp -r /var/app/node_modules /app/ && npm run "${COMMAND}"
+    return
+fi
+
+npm run "${COMMAND}"
